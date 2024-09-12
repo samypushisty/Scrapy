4335fbeb4e3ca8a09853f4e555dd8a2283a59279
OmegaKaban <pomidorik228@gmail.com>
2024-09-12 16:40:56 +0300

    somepip-8 update

--- firstp/firstp/spiders/example.py
+++ firstp/firstp/spiders/example.py
@@ -1,15 +1,12 @@
-from pathlib import Path
 import scrapy
 
 
 class ExampleSpider(scrapy.Spider):
     # 145
-    count_pages = 145
-    href = ""
+    count_pages = 10
     name = "example"
     allowed_domains = ["catalog.onliner.by"]
     start_url = "https://catalog.onliner.by/pan"
-
 
     def start_requests(self):
         start_url_in = self.start_url
@@ -24,21 +21,19 @@ class ExampleSpider(scrapy.Spider):
                               "catalog-form__link_font-weight_semibold "
                               "catalog-form__link_nodecor']")
         for pan in pans:
-            self.href = pan.xpath(".//@href").get().strip()
-            yield response.follow(pan.xpath(".//@href").get().strip(), callback=self.parse_page,
-                             cb_kwargs=dict(href=pan.xpath(".//@href").get().strip()))
-            # yield {
-            # 'title': pan.xpath("text()").get().strip(),
-            # 'href': pan.xpath(".//@href").get().strip()
-            # }
+            yield response.follow(
+                pan.xpath(".//@href").get().strip(),
+                callback=self.parse_page,
+                cb_kwargs=dict(href=pan.xpath(".//@href").get().strip())
+            )
 
-    def parse_page(self, response,href):
-        yield {"title":response.xpath("//h1[@class='catalog-masthead__title js-nav-header']/text()").get().strip(),
-               "href":href,
-               "price":response.xpath("//a[@class='offers-description__link offers-description__link_nodecor js-description-price-link']/text()").get().strip()[:-3]+"р",
-               "discription":response.xpath("//div[@class='offers-description__specs']/p/text()").get().strip()
+    def parse_page(self, response, href):
+        yield {"title": response.xpath("//h1[@class='catalog-masthead__title js-nav-header']/text()").get().strip(),
+               "href": href,
+               "price": response.xpath("//a[@class='offers-description__link "
+                                       "offers-description__link_nodecor "
+                                       "js-description-price-link']/text()").get().strip()[:-3]+"р",
+               "discription": response.xpath("//div[@class='offers-description__specs']/p/text()").get().strip()
                }
-
-
 
 # scrapy crawl example -o titles.json
